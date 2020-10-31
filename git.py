bfa549f2b938d604b2fa92d3d0c16d71a6457558
HAKANMAZI <hak122hak@gmail.com>
2020-10-31 16:49:37 +0300

    Update and rename git.txt to git.py

---
+++ git.py
@@ -0,0 +1,31 @@
+git clone https://github.com/HAKANMAZI/team_cobanov.git
+
+#create branch
+    git checkout -b hakan-ekle 
+#show branch
+    git branch
+    
+#readme dosyası güncellendi    
+    git add Readme.md
+    
+#değişikliği mesaj ile gönder
+    git commit -m "Hakan eklendi"
+    
+#durum kontrol
+    git status
+    
+#github'a gönder 
+    git push origin hakan-ekle
+
+git add .
+git commit -am "test"
+git push
+
+rm -f .git/index.lock
+    Another git process seems to be running in this repository, e.g.
+    an editor opened by 'git commit'. Please make sure all processes
+    are terminated then try again. If it still fails, a git process
+    may have crashed in this repository earlier:
+    remove the file manually to continue.
+
+git pull
