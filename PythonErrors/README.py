3990c9ba6dc94f061430bc5402ca01dbcd8bae84
HAKANMAZI <hak122hak@gmail.com>
2021-01-27 22:43:49 +0300

    Update README.py

--- PythonErrors/README.py
+++ PythonErrors/README.py
@@ -3,3 +3,11 @@ import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR'
 Image.open('twitterkey.png')
 print(pytesseract.image_to_string(Image.open('twitterkey.png')))
+
+'''Error
+  File "c:\users\hknmz\appdata\local\programs\python\python38\lib\subprocess.py", line 854, in __init__
+    self._execute_child(args, executable, preexec_fn, close_fds,
+  File "c:\users\hknmz\appdata\local\programs\python\python38\lib\subprocess.py", line 1307, in _execute_child
+    hp, ht, pid, tid = _winapi.CreateProcess(executable, args,
+PermissionError: [WinError 5] Erişim engellendi
+'''
